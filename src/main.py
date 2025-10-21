f51015bec5de524eb28040f2f13e8755b7084ac9
spinto <3627896+spinto@users.noreply.github.com>
2025-10-21 10:49:48 +0000

    reviewed mandatory STAC metadata

--- src/main.py
+++ src/main.py
@@ -17,7 +17,31 @@ import re
 import shutil
 
 #Setup FastAPI
-app = FastAPI(title="Registration Gateway",description="This application acts as a gateway for OGC Records - Transactions API, capturing requests and ensuring the assets contained in the Items to be registered are valid an stored into a proper storage location.",version="0.0.1")
+app = FastAPI(title="Registration Gateway",description="""This API interface allows you to ingest at the same time metadata and related data assets. 
+
+Auhtorized users can POST a new STAC Item into a Collection, linking assets uploaded into S3 stagein bucket associated to the Collection. The assets will be moved to the system long-term storage and the metadata updated and ingested into the sytstem clatalogue for long term data preservation.
+
+The the STAC Items shall follow the following profile.
+
+| Element | Type | Description |
+| ---------------- | -------------------- | ----------- |
+| type | string | **REQUIRED.** Must be set to \`Feature\`.|
+| stac_version | string | **REQUIRED.** The STAC version. Recommended to be set to \`1.0.0\` |
+| stac_extensions | \[string] | **REQUIRED** A list of extension identifiers the Collection implements. Must contain at least the [File extension](https://stac-extensions.github.io/file/v2.1.0/schema.json) |
+| id| string | **REQUIRED.** Item identifier. The ID should be unique within the Collection that contains the Item. The id metadata can contain only the [a-zA-Z0-9._-] characters and shall have maximum 100 characters. |
+| geometry | GeoJSON Geometry Object or null | **REQUIRED.** Defines the full footprint of the asset represented by this item, formatted according to RFC 7946, [section 3.1](https://tools.ietf.org/html/rfc7946#section-3.1) if a geometry is provided or [section 3.2](https://tools.ietf.org/html/rfc7946#section-3.2) if *no* geometry is provided. |
+| bbox | \[number] | **REQUIRED if `geometry` is not `null`, prohibited if `geometry` is `null`.** Bounding Box of the asset represented by this Item, formatted according to [RFC 7946, section 5](https://tools.ietf.org/html/rfc7946#section-5).                                                                            |
+| properties.start_datetime | string | **REQUIRED.** The first or start date and time for the resource, in UTC. It is formatted as `date-time` according to [RFC 3339, section 5.6](https://tools.ietf.org/html/rfc3339#section-5.6). |
+| properties.end_datetime   | string | **REQUIRED.** The last or end date and time for the resource, in UTC. It is formatted as `date-time` according to [RFC 3339, section 5.6](https://tools.ietf.org/html/rfc3339#section-5.6).    |
+| properties.datetime | string | The searchable date and time of the assets, which must be in UTC. If not specified or null, it will be set to `start_datetime`. |
+| assets | Map<string, [Asset Object](https://github.com/radiantearth/stac-spec/blob/master/collection-spec/collection-spec.md#assets)>| **REQUIRED.** Dictionary of asset objects that can be downloaded, each with a unique key. Further constrains on asset values are below |
+| assets[].href | string | **REQUIRED.** Each asset of the STAC Item containing an URI as \`href\` is ingored and will be posted into the Catalogue as-is. Each asset of the STAC Item containing a local path as \`href\` is transferred from the stagein area to the main storage area. It shall thus have: An \`href\` pointing to the relative path of the asset binary file into the S3 stagein bucket associated to the Collection. An \`href\` filename containing only the [a-zA-Z0-9._-] characters (the rest of the filename path is ignored). The path cannot point to a directory, only a file. The filename shall have maximum 100 characters. An \`href\` filename unique among all the STAC Item assets (two assets in the same STAC item cannot have the same filename, even if they have different relative paths)  |
+| assets[].type | string | **REQUIRED.** [Media type](#media-types) of the asset. It is highly reommended to use Cloud-native formats such as COG or Zarr. See the [common media types](../best-practices.md#co) in the best practice doc for commonly used asset types. |      |
+| assets[].roles | \[string] | **REQUIRED.** The [semantic roles](#roles) of the asset, similar to the use of `rel` in links. |
+| assets[].file:checksum | string | It is strongly recommended to provide the file checksum. The hashes are self-identifying hashes as described in the [Multihash specification](https://github.com/multiformats/multihash) and must be encoded as hexadecimal (base 16) string with lowercase letters. |
+| assets[].file:size | integer | **REQUIRED.** The file size, specified in bytes. If a local asset is specified, this is checked. |
+
+""",version="0.0.2")
 
 #Get current script execution path
 CURPATH = os.path.dirname(os.path.realpath(__file__))
@@ -77,17 +101,7 @@ async def validation_exception_handler(request, exc):
 
 To access it, you need to be registered as Data Provider and authorized to publish in the Catalogue collection (_{collectionId}_ in the API endpoint path).
 
-The POST request body needs to contain the STAC Item or the STAC ItemCollection to be published.
-
-The STAC Item or STAC ItemCollection to be published needs to respect the following constraints:
-- It needs to be a valid STAC Item, according to the [STAC Item Specifications](https://github.com/radiantearth/stac-spec/blob/master/item-spec/item-spec.md), or a valid STAC ItemCollection, according to the [STAC ItemCollection Specifications](https://github.com/radiantearth/stac-api-spec/tree/release/v1.0.0/fragments/itemcollection), containing valid STAC Items inside
-- If a _collection_ metadata is present, it needs to contain the same value as _{collectionId}_. If it is not present, it will be added with the same value as _{collectionId}_
-- The _id_ metadata can contain only the [a-zA-Z0-9._-] characters and shall have maximum 100 characters.
-- Each asset of the STAC Item containing a URI as "href" is ignored and will be posted into the Catalogue as-is
-- Each asset of the STAC Item containing a local path as "href" is ingested. It shall thus have:
-  - An asset href pointing to the relative path of the asset binary file in the S3 stagein bucket associated to the Collection
-  - An asset href filename containing only the [a-zA-Z0-9._-] characters (the rest of the filename path is ignored). The path cannot point to a directory, only a file. The filename shall have maximum 100 characters.
-  - An asset href filename unique among all the STAC Item assets (two assets in the same STAC item cannot have the same filename, even if they have different relative paths)
+The POST request body needs to contain the STAC Item or the STAC ItemCollection to be published. The STAC Item shall follow the profile and the constrains described above.
 
 The API response will contain, in case of success, the ingested STAC Item or a STAC ItemCollection (containing the ingested STAC Item) as present in the Catalogue, thus with
 - Additional required metadata, including a link to the STAC Collection
@@ -104,15 +118,21 @@ Examples of API request and response are provided below. Please note that this A
       "type": "Feature",
       "stac_version": "1.0.0",
       "stac_extensions": [
-        "https://stac-extensions.github.io/alternate-assets/v1.1.0/schema.json",
-        "https://stac-extensions.github.io/storage/v1.0.0/schema.json"
+        "https://stac-extensions.github.io/file/v2.1.0/schema.json"
       ],
       "id": "S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600",
+      "properties": {
+        "start_datetime": "2015-05-19T12:00:00.000000Z",
+        "end_datetime": "2015-05-20T12:00:00.000000Z"
+      },
       "assets": {
         "PRODUCT": {
           "href": "http://myserver.com/d/2015/05/19/S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600/S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600.tgz",
           "title": "Product",
           "type": "application/octet-stream",
+          "roles": ["data"],
+          "file:checksum": "90e4021044a8995dd50b6657a037a7839304535b",
+          "file:size": 153600
         }
       },
       "collection": "PRR_TEST",
@@ -126,13 +146,19 @@ Examples of API request and response are provided below. Please note that this A
         "PRODUCT": {
           "href": "http://myserver.com/d/2015/05/19/S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600/S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600.tgz",
           "title": "Product",
-          "type": "application/octet-stream"
+          "type": "application/octet-stream",
+          "roles": ["data"],
+          "file:checksum": "90e4021044a8995dd50b6657a037a7839304535b",
+          "file:size": 153600
         }
       },
       "id": "S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600",
+      "properties": {
+        "start_datetime": "2015-05-19T12:00:00.000000Z",
+        "end_datetime": "2015-05-20T12:00:00.000000Z"
+      },
       "stac_extensions": [
-        "https://stac-extensions.github.io/alternate-assets/v1.1.0/schema.json",
-        "https://stac-extensions.github.io/storage/v1.0.0/schema.json"
+        "https://stac-extensions.github.io/file/v2.1.0/schema.json"
       ],
       "stac_version": "1.0.0",
       "type": "Feature",
@@ -157,13 +183,13 @@ Examples of API request and response are provided below. Please note that this A
     }}}}}, 
     422: {"description": "Validation error. Failed to add one or more items. Error in the response","content":{"application/json":{"examples":{"Item":{"value":
       {"id": "the id of the item which failed to be ingested if determined",
-       "failure_reason": "a test describing the error"}
+       "failure_reason": "a text describing the error"}
     },"ItemCollection":{"value":
 {
   "type": "FeatureCollection",
   "features": [
       {"id": "the id of the item which failed to be ingested if determined",
-       "failure_reason": "a test describing the error"}
+       "failure_reason": "a text describing the error"}
   ]
 }
     }}}}}
@@ -176,18 +202,21 @@ async def collection_items_post_request(
     "type": "Feature",
     "stac_version": "1.0.0",
     "stac_extensions": [
-      "https://stac-extensions.github.io/alternate-assets/v1.1.0/schema.json",
-      "https://stac-extensions.github.io/storage/v1.0.0/schema.json"
+      "https://stac-extensions.github.io/file/v2.1.0/schema.json"
     ],
     "id": "S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600",
     "properties": {
-      "datetime": "2015-05-19T12:00:00.000000Z",
+      "start_datetime": "2015-05-19T12:00:00.000000Z",
+      "end_datetime": "2015-05-20T12:00:00.000000Z"
     },
     "assets": {
       "PRODUCT": {
         "href": "the/path/is/not/important/S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600.tgz",
         "title": "Product",
         "type": "application/octet-stream",
+        "roles": ["data"],
+        "file:checksum": "90e4021044a8995dd50b6657a037a7839304535b",
+        "file:size": 153600
       }
     }
   }},
@@ -199,18 +228,21 @@ async def collection_items_post_request(
                 "PRODUCT": {
                     "href": "the/path/is/not/important/S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600.tgz",
                     "title": "Product",
-                    "type": "application/octet-stream"
+                    "type": "application/octet-stream",
+                    "roles": ["data"],
+                    "file:checksum": "90e4021044a8995dd50b6657a037a7839304535b",
+                    "file:size": 153600
                 }
             },
             "id": "S3A_OPER_AUX_GNSSRD_POD__20171212T193142_V20160223T235943_20160224T225600",
             "stac_extensions": [
-                "https://stac-extensions.github.io/alternate-assets/v1.1.0/schema.json",
-                "https://stac-extensions.github.io/storage/v1.0.0/schema.json"
+              "https://stac-extensions.github.io/file/v2.1.0/schema.json"
             ],
             "stac_version": "1.0.0",
             "type": "Feature",
             "properties": {
-              "datetime": "2015-05-19T12:00:00.000000Z",
+              "start_datetime": "2015-05-19T12:00:00.000000Z",
+              "end_datetime": "2015-05-20T12:00:00.000000Z"
             },
         }
     ],
@@ -273,21 +305,20 @@ async def add_item_to_collection(assets_source: str, assets_dest: str, stac_dest
     i['geometry']=None
   if 'properties' not in i:
     return {"id":i['id'],"failure_reason":"Missing required property field from the STAC JSON"}
+  if 'start_datetime' not in i['properties']: return {"id":i['id'],"failure_reason":"Missing required start_datetime from the STAC JSON"}
+  if 'end_datetime' not in i['properties']: return {"id":i['id'],"failure_reason":"Missing required end_datetime from the STAC JSON"}
   if 'datetime' not in i['properties'] or i['properties']['datetime'] is None:
-    if 'start_datetime' in i['properties']:
-      item_datetime_str=i['properties']['start_datetime']
-    else:
-      return {"id":i['id'],"failure_reason":"datetime or start_datetime properties are mandatory"}
-  else:
-    item_datetime_str=i['properties']['datetime']
-  try:
-    item_datetime_obj=dt.datetime.fromisoformat(item_datetime_str)
-  except Exception as e:
-    return {"id":i['id'],"failure_reason":"Failed to parse product date time. {item_datetime_str} is an invalid ISO time"}
-
+    i['properties']['datetime']=i['properties']['start_datetime']
+  for a in ['start_datetime','end_datetime','datetime']:
+    try:
+      item_datetime_str_orig=i['properties'][a]
+      item_datetime_obj=dt.datetime.fromisoformat(item_datetime_str_orig)
+      item_datetime_str_new=item_datetime_obj.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception as e:
+      return {"id":i['id'],"failure_reason":f"Failed to parse product date time. {item_datetime_str_orig} is an invalid ISO time"}
   #Extract all the local assets (and check that they are available)
   #Destination for the asset is calculated using the collection and the asset start time if present. This is to not overload the file system with too many assets and have one unique way of representing assets in the datastore.
-  assets_base_date=item_datetime_obj.strftime(f'%Y{os.sep}%m{os.sep}%d')
+  assets_base_date=dt.datetime.fromisoformat(i['properties']['datetime']).strftime(f'%Y{os.sep}%m{os.sep}%d')
   assets_base_path=os.path.join(assets_base_date,i["id"])
   assets_to_move_src=[]
   assets_to_move_dst=[]
@@ -295,8 +326,13 @@ async def add_item_to_collection(assets_source: str, assets_dest: str, stac_dest
     assets_dict=i['assets']
     for asset_key in assets_dict:
       asset=assets_dict[asset_key]
+      #Check mandatory metadata for the asset are provided
+      mandatory_metadata={'href':str,'type':str,'roles':list,'file:size':int}
+      for a in mandatory_metadata:
+        if a not in asset: return {"id":i['id'],"failure_reason":f"{asset_key} asset does not have the mandatory '{a}' metadata."}
+        if not isinstance(asset[a],mandatory_metadata[a]): return {"id":i['id'],"failure_reason":f"{asset_key} asset metadata '{a}' is invalid."}
       #Skip URLs and assets who do not have HREF from the assets to be moved
-      if 'href' in asset and '://' not in asset['href']:
+      if '://' not in asset['href']:
         #Check if the assets exist in the storage
         staging_asset_path=os.path.join(assets_source,asset['href'])
         if not os.path.exists(staging_asset_path):
@@ -310,6 +346,9 @@ async def add_item_to_collection(assets_source: str, assets_dest: str, stac_dest
         staging_asset_filename=os.path.basename(staging_asset_path)
         if not valid_id_match(staging_asset_filename,lenmax=100):
           return {"id":i['id'],"failure_reason":f"Asset {asset_key} file name is invalid. Only [a-zA-Z0-9._-] are allowed"}
+        #Check the file size matches the one in the metadata
+        staging_asset_filesize=os.path.getsize(staging_asset_path)
+        if staging_asset_filesize != asset['file:size']: return {"id":i['id'],"failure_reason":f"Asset {asset_key} is invalid. File size does not match the one of the file in the S3 stagein path"} 
         #Determine destination for the asset in the data store, this is used for both the access URL and the datastore destination path below
         ds_asset_path=os.path.join(assets_base_path,staging_asset_filename)
         #Rewrite the asset URL to make it point to the stagein
@@ -323,7 +362,18 @@ async def add_item_to_collection(assets_source: str, assets_dest: str, stac_dest
         if staging_asset_path_dest in assets_to_move_dst:          
           return {"id":i['id'],"failure_reason":f"Asset {asset_key} file name is not unique. Another asset in the product has the same file name"}
         assets_to_move_dst.append(staging_asset_path_dest)
-  
+    #Check at least one asset with role 'data' is provided
+    data_is_present=False
+    for asset_key in assets_dict:
+      asset=assets_dict[asset_key]
+      if 'data' in asset['roles']:
+        data_is_present=True
+        break
+    if data_is_present == False:
+      return {"id":i['id'],"failure_reason":f"At least one asset with role 'data' is mandatory."}
+  else:
+    #Error, one asset with data is mandatory
+    return {"id":i['id'],"failure_reason":f"No asset provided. At least one asset with role 'data' is mandatory."}
   #Post the STAC Item to the catalogue
   #Construct catalogue request
   stac_item=json.dumps(i).encode("utf-8")
